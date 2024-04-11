78d468ca54ec6ac82f7deb3df1eb3f828f55eec9
Daniel Antunes <antunesdaniel083@gmail.com>
2024-04-11 11:20:59 -0300

    Tirando a importação que não precisa

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 import os
 from openpyxl import load_workbook
